f5f47099da31ec729dec6c972746e040b5c50586
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2025-10-29 22:30:14 +0330

    fixed start backend

--- start-backend.bat
+++ start-backend.bat
@@ -4,13 +4,13 @@ setlocal EnableDelayedExpansion
 echo Starting YOLO Backend Service... >> C:\Users\pc\object-detection\backend-service.log 2>&1
 
 REM Set Python paths for service environment
-set "PATH=C:\ProgramData\anaconda3\envs\yolo_cpu;C:\ProgramData\anaconda3\envs\yolo_cpu\Scripts;C:\ProgramData\anaconda3\envs\yolo_cpu\Library\bin;%PATH%"
+set "PATH="C:\Users\pc\.conda\envs\yolo_cpu;C:\Users\pc\.conda\envs\yolo_cpu\Scripts;C:\Users\pc\.conda\envs\yolo_cpu\Library\bin;%PATH%"
 set "CONDA_DEFAULT_ENV=yolo_cpu"
 set "CONDA_PREFIX=C:\ProgramData\anaconda3\envs\yolo_cpu"
 
 REM === Env vars for Django & your app ===
 set "DJANGO_SETTINGS_MODULE=yolo_api.settings"
-set "YOLO_MODEL_PATH=C:\Users\pc\object-detection\backend\weights\best.pt"
+set "YOLO_MODEL_PATH=C:/Users/pc/object-detection/backend/weights/best.pt"
 set "DEFAULT_YOLO_DEVICE=cpu"
 set "CUDA_VISIBLE_DEVICES="
 
