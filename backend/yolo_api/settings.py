77bb2cae0c4f0e3abba24dab9aded9d7097f0514
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2025-10-31 14:50:57 +0330

    CRF removed 2.

--- backend/yolo_api/settings.py
+++ backend/yolo_api/settings.py
@@ -7,7 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'change-me-in-prod'
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "192.168.144.21",
+    "localhost",
+    "127.0.0.1", 
+    "0.0.0.0",
+    "::1",]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
